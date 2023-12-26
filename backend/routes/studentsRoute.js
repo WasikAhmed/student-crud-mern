d90f7eee736434835bad4d93bf8e6efb56b57a2a
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-12-26 23:41:11 +0600

    Added Student model, routes for CRUD operations, and database connection
    This message succinctly captures the essence of the changes made in the respective files:
    
    - Introduction of a Student model with specified schema fields using Mongoose.
    - Creation of routes for handling GET and POST requests related to student data.
    - Establishment of a database connection to MongoDB using Mongoose in the server file (server.js).

---
+++ backend/routes/studentsRoute.js
@@ -0,0 +1,48 @@
+const Student = require("../models/studentModel");
+
+const express = require("express");
+const router = express.Router();
+
+// Route for get all students
+router.get('/', async (req, res) => {
+    try {
+        const students = await Student.find();
+
+        return res.status(200).json({
+            count: students.length,
+            data: students
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+// Route for create a new student
+router.post('/', async (req, res) => {
+    try {
+        if(
+            !req.body.name || 
+            !req.body.department || 
+            !req.body.cgpa
+        ) {
+            return res.status(400).send({
+                message: "Send all required fields: name, department, cgpa"
+            })
+        }
+        const newStudent = new Student({
+            name: req.body.name,
+            department: req.body.department,
+            cgpa: req.body.cgpa
+        });
+        const student = await Student.create(newStudent);
+
+        return res.status(201).json({ message: "Student created successfully", student});
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send({ message: error.message });
+    }
+});
+
+module.exports = router;
