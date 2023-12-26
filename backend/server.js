ccb01d803c97a74514602fe2e4e51322f157ba1e
Wasik Ahmed Apon <22-47698-2@student.aiub.edu>
2023-12-26 21:47:49 +0600

    Initialize backend

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
